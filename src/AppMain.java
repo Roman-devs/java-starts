d6c133f8e82ef175dc846fc58d880e0772116f52
Roman Kuite <roman.kuite@gmail.com>
2021-01-19 17:25:55 +0100

    Add first java script

---
+++ src/AppMain.java
@@ -0,0 +1,12 @@
+public class AppMain {
+    public static void main(String[] args) {
+        // Initializing variables
+        int result1 = 15;
+        int result2 = 14;
+        int finalresult = result1 + result2;
+        // Defining the answer variable through previously initialized variables
+        String answer = "The final result is" + " " + finalresult;
+        // Output/Print of variable 'answer' in terminal
+        System.out.println(answer);
+    }
+}
